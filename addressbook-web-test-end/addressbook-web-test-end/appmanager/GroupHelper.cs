0c40ffe20c8c1dc6e95d8146ff298795cedeb1c5
Alderunna <violetfrigate@gmail.com>
2023-04-20 15:31:08 +0700

    Не работает сортировка

--- addressbook-web-test-end/addressbook-web-test-end/appmanager/GroupHelper.cs
+++ addressbook-web-test-end/addressbook-web-test-end/appmanager/GroupHelper.cs
@@ -89,7 +89,7 @@ namespace WebAddressbookTests
 
         public GroupHelper SelectGroup(int index)
         {
-            driver.FindElement(By.XPath("//div[@id='content']/form/span[" + index + "]/input")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']/form/span[" + (index+1) + "]/input")).Click();
             return this;
         }
 
@@ -111,6 +111,16 @@ namespace WebAddressbookTests
             return this;
         }
 
-
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            manager.Navigator.GoToGroupsPage();
+            ICollection<IWebElement> elements = driver.FindElements(By.CssSelector("span.group"));
+            foreach (IWebElement element in elements) 
+            {
+                groups.Add(new GroupData(element.Text));
+            }
+            return groups;
+        }
     }
 }
--- addressbook-web-test-end/addressbook-web-test-end/tests/GroupCreationTests.cs
+++ addressbook-web-test-end/addressbook-web-test-end/tests/GroupCreationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Collections.Generic;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -19,7 +20,15 @@ namespace WebAddressbookTests
             group.Header = "bbb";
             group.Footer = "ccc";
 
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
             app.Groups.Create(group);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(group);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
         }
 
 
@@ -29,8 +38,16 @@ namespace WebAddressbookTests
             GroupData group = new GroupData("");
             group.Header = "";
             group.Footer = "";
-            
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
             app.Groups.Create(group);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(group);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
         }
     }
 }
